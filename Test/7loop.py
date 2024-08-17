61512674139a2e8df13859f26dcbc23bec23ff08
Anila11k <anilakunjumon88@gmail.com>
2024-08-17 09:10:57 +0530

    Add files via upload

---
+++ Test/7loop.py
@@ -0,0 +1,140 @@
+# i=1
+# while i<6:
+#     print(i)
+#     i+=1
+
+# i=5
+# while i>1:
+#     print(i)
+#     i-=1
+
+
+# i=4
+# while i<14:
+#     print(i)
+#     i+=1
+
+
+
+# i=20
+# while i<40:
+#     print(i)
+#     i+=1
+
+
+
+# i=5
+# while i<12:
+#     print(i)
+#     if (i==8):
+#         break
+#     i+=1
+
+
+# i=4
+# while i<12:
+#     print(i)
+#     if(i==7):
+#         break
+#     i+=1
+
+
+
+
+# i=14
+# while i<20:
+#     print(i)
+#     if(i==15):
+#         break
+#     i+=1
+
+
+# i=0
+# while i<7:
+#     i+=1
+#     if i==4:
+#         continue
+#     print(i)
+
+
+# i=1
+# while i<12:
+#     i+=1
+#     if i==10:
+#         continue
+#     print(i)
+
+
+
+
+# fruits=["apple","banana","cherry",]
+# for x in fruits:
+#     print(x)
+
+# fruits=["apple","banana","cherry"]
+# for x in fruits:
+#     print(x)
+    
+
+# fruits=["apple","banana","cherry"]
+# for x in fruits:
+#     print(x)
+#     if x=="banana":
+#         break
+
+
+
+# fruits=["apple","banana","cherry"]
+# for x in fruits:
+#     print(x)
+#     if x=="apple":
+#         break
+
+
+
+
+
+
+# fruits=["apple","banana","cherry"]
+# for x in fruits:
+#     if x=="apple":
+#         continue
+#     print(x)
+
+
+# for x in range(10):
+#     print(x)
+
+# for x in range(2,6):
+#     print(x)
+
+# for x in range(2,30,3):
+#     print(x)    
+
+
+# for x in range(2,12,2):
+#     print(x)
+        
+
+
+# for x in "banana":
+#     print(x)
+
+# for x in "anila":
+#     print(x)
+
+
+
+ 
+     
+ 
+
+
+
+
+   
+
+
+
+
+   
