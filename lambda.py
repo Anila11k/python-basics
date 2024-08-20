671cf47291133a7ef955fead5dc4dc1d7fc779da
Anila11k <anilakunjumon88@gmail.com>
2024-08-20 13:06:31 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,80 @@
+# anonymus function
+# ---------------------
+# #* declare a lamda function
+# greet=lambda: print("hello world")
+# greet()
+
+# greet_user=lambda name:print('hey there,',name)
+# greet_user ("dellah")
+
+
+# greet_user=lambda name:print('hey there,',name)
+# greet_user ("anila")
+
+
+# list1=[1,2,3,4,5,6]
+# sqr =list(map(lambda x:x**2,list1))
+# print(sqr)
+
+# list1= [2,4,9,6,7,3]
+# sqr=list(map(lambda x:x**2,list1))
+# print(sqr)
+
+# newlist=[1,2,3,4,5,6]
+# odd =list(filter(lambda x:x%2!=0,newlist))
+# print(odd)
+
+
+# list1=[1,2,3,4]
+# from functools import reduce
+# prdct=reduce(lambda x,y:x*y,list1)
+# print(prdct)
+
+
+# def my_function():
+#     print("Hello from a function")
+# my_function()
+#      Hello from a function    
+
+
+# def my_function(fname):
+#     print(fname+"john")
+# my_function("emil")
+# my_function("linus")
+# my_function("linus")
+
+
+# def my_function(fname):
+#     print(fname+" kunjumon")
+
+# my_function("akhila")
+# my_function("girija")
+# my_function("anila")
+
+
+
+# def add_numbers(a,b):
+#     sum=a+b
+#     print('sum:',sum)
+#     add_numbers(2,3)
+
+
+# def add_numbers (a=21,b=8):
+#     sum=a+b
+#     print('sum:',sum)
+# add_numbers(2,3)
+# add_numbers(a=2)
+# add_numbers()
+
+# def display_info (first_name,last_name):
+#     print('first_ name:',first_name)
+#     print('last name:',last_name)
+# display_info(last_name='cartman',first_name='eric')   
+
+# def find_sum(*numbers):
+#     result=0
+#     for num in numbers:
+#         result=result+num
+#     print("sum=",result)
+# find_sum(1,2,3)
+# find_sum(4,9)
