71466350a8d3f6f3faea51723d0b9f06cc3b7d91
Anila11k <anilakunjumon88@gmail.com>
2024-08-22 13:00:36 +0530

    Add files via upload

---
+++ constructor.py
@@ -0,0 +1,30 @@
+# class employee:
+#   def __init__(self,name="bhavana",age=24):
+#      self.name=name
+#      self.age=age
+#   def displayEmployee(self):
+#        print("name:",self.name,",age:",self.age)
+        
+# e1=employee()
+# e2=employee("bharat",25)
+     
+# e1.displayEmployee()
+# e2.displayEmployee()
+
+
+# class student:
+#     def __init__(self,name="anila",age=27):
+#         self.name=name
+#         self.age=age
+#     def displaystudent(self):  
+#           print("name:",self.name,",age:",self.age) 
+
+# s1=student()
+# s2=student("nithi",27)
+
+# s1.displaystudent()
+# s2.displaystudent()
+
+
+        
+      
