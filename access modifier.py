4556207fb3203d5e24bb77c2d221c89f75e02e24
Anila11k <anilakunjumon88@gmail.com>
2024-08-26 13:06:34 +0530

    Add files via upload

---
+++ access modifier.py
@@ -0,0 +1,29 @@
+# 1.public
+# 2.private
+# 3.protected
+
+#public modifier:-
+
+# class student:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#     def display(self):
+#         print("name:",self.name)
+#         print("age:",self.age)
+# S= student("john",20) 
+# S.display()   
+
+
+# private modifier
+
+
+# class bankaccount:
+#     def __init__(self,accountnumber,balance):
+#      self.accountnumber=accountnumber
+#      self.balance=balance
+#     def _dislay_balance(self):
+#        print("balance:",self_balance) 
+# b=bankaccount(123654899)
+# b=_display_balance()         
+
---
+++ super function.py
@@ -0,0 +1,20 @@
+# class Emp():
+#     def __init__(self,id,name,add):
+#         self.id=id
+#         self.name=name
+#         self.add=add
+# class Freelance(Emp):
+#     def __init__(self,id,name,add,emails):
+#         super().__init__(id, name, add) 
+#         self.emails=emails
+       
+# Emp_1=Freelance(103,"suraj kr gupta","noida","kkk@gmail.com")
+
+# print('The id is:',Emp_1.id)
+# print('The name is:',Emp_1.name)
+# print('The address is:',Emp_1.add)
+# print('The email is:',Emp_1.emails)
+
+
+
+
