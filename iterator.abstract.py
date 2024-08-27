a20ee5fdb01c67babe43856bfcd8875c72bcb5a6
Anila11k <anilakunjumon88@gmail.com>
2024-08-27 13:06:19 +0530

    Add files via upload

---
+++ iterator.abstract.py
@@ -0,0 +1,97 @@
+
+# iterator vs iterable
+
+# mytuple=("apple","banana","cherry")
+# myit=iter(mytuple)
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+
+# mystr ="banana"
+# myit = iter(mystr)
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+
+
+# class Mynumbers:
+#     def __iter__(self):
+#      self.a= 1
+#      return self
+#     def __next__(self):
+#      x=self.a
+#      self.a +=1 
+#      return x
+# myclass=Mynumbers()
+# myiter =iter(myclass)
+
+# print(next(myiter))
+# print(next(myiter))
+# print(next(myiter))
+# print(next(myiter))
+# print(next(myiter))
+
+
+# abstract class in python
+
+# class Car:
+#     def mileage(self):
+#         pass
+# class Tesla(Car):
+#     def mileage(self):
+#         print("The mileage is 30kmph")
+
+# class Suzuki(Car):
+#     def mileage(self):
+#         print("The mileage is 25kmph")
+
+# class Duster(Car):
+#     def mileage(self):
+#         print("the mileage is 24kmph")        
+# class Renault(Car):
+#     def mileage(self):    
+#         print("The mileage is 27kmph")    
+
+# t =Tesla()
+# t.mileage()
+# s=Suzuki()
+# s.mileage()
+# d=Duster()
+# d.mileage()
+
+# r=Renault()
+# r.mileage()     
+
+
+
+# class Students:
+#     def division(self):
+#         pass
+# class Anila(Students):
+#     def division(self):
+#         print("division is A")
+# class Akhila(Students):
+#     def division(self):
+#         print("division is B")      
+# class Kunjumon(Students):
+#     def division(self):
+#         print("division is C")
+# class Girija(Students):
+#     def division(self):
+#         print("division is D")                  
+
+# a=Anila()
+# a.division()
+# b=Akhila()
+# b.division()
+# k=Kunjumon()
+# k.division()
+
+# g=Girija()
+# g.division()
+
+
+
