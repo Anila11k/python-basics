b08fb6990936c933c3ec89701b4480b01d81cad8
Anila11k <anilakunjumon88@gmail.com>
2024-09-04 12:50:31 +0530

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,70 @@
+# from tkinter import*
+# root=Tk()
+# root.mainloop()
+
+
+
+# from tkinter import*
+# root=Tk()
+# root.title("welcome to python lobby")
+# root.geometry('300x200')
+# root.mainloop()
+
+
+#  LABEL
+# '''''''
+
+# from tkinter import*
+# root=Tk()
+# root.title("welcome to python Lobby")
+
+# root.geometry('250x200')
+
+# lbl= Label(root,text = "we are python lobby lan")
+# lbl.pack()
+# root.mainloop()
+
+
+#  BUTTONS
+# ''''''''''
+
+
+# from tkinter import*
+# root=Tk()
+# root.title("welcome to python Lobby")
+# def clicked():
+#     print("i am clicked")
+# btn = Button(root, text="Click me", command=clicked)
+# btn.pack()
+# root.geometry('250x200')
+# root.mainloop()
+
+
+#  ENTRY
+# ''''''''
+
+# from tkinter import*
+# root=Tk()
+# root.title("welcome to python Lobby")
+# root.geometry('250x200')
+# entry=Entry(root)
+# entry.pack()
+# root.mainloop()
+
+#  IMAGE WIDTH
+# '''''''''''''''
+
+# from tkinter import*
+# from tkinter import ttk
+
+# root=Tk()
+# label = Label (root,text="This is image").pack(side=TOP,pady=10)
+
+# path= PhotoImage(file="c:/Users/Python/Downloads/car-967387_640.png")
+# label_image=Label(root,image=path,width=400, height=400)
+# label_image.pack()
+
+# root.geometry('600x440')
+# root.title("pythonLobby.com")
+# root.mainloop()
+
