f4880ad2729453ee246280d224c26f86bfc8f246
Anila11k <anilakunjumon88@gmail.com>
2024-09-05 12:49:49 +0530

    Add files via upload

---
+++ checkboxes.py
@@ -0,0 +1,26 @@
+# from tkinter import*
+# root=Tk()
+# root.title("python Lobby")
+# w=Label(root,text='PythonLobby.com',font="60")
+# w.pack()
+
+# Checkbox1 =IntVar()
+# Checkbox2=IntVar()
+
+# Button0 =Checkbutton(root,text="Learning",
+#                     variable=Checkbox1,
+#                     onvalue=1,
+#                     offvalue=0,
+#                     height=3,
+#                     width =12)
+# Button1 =Checkbutton(root,text="Tutorial",
+#                     variable=Checkbox2,
+#                     onvalue=1,
+#                     offvalue=0,
+#                     height=3,
+#                     width=12)
+
+# Button0.pack()
+# Button1.pack()
+# root.geometry('320x200')
+# mainloop()
---
+++ scrollbar width.py
@@ -0,0 +1,28 @@
+# from tkinter import*
+# root=Tk()
+
+# textbox=Text(root,width=40,height=13, wrap=WORD)
+# textbox.grid(row=0, column=0, )
+
+# scroll=Scrollbar(root,orient=VERTICAL,command= textbox.yview)
+# scroll.grid(row=0,column=1, sticky=N+S)
+# textbox.config(yscrollcommand=scroll.set)
+
+# root.geometry("350x220")
+# root.title("PythonLobby.com")
+# root.mainloop()
+
+
+# from tkinter import*
+# root=Tk()
+
+# textbox=Text(root,width=40,height=13, wrap=WORD)
+# textbox.grid(row=0, column=0, )
+
+# scroll=Scrollbar(root,orient=HORIZONTAL,command= textbox.xview)
+# scroll.grid(row=0,column=1, sticky=N+W)
+# textbox.config(xscrollcommand=scroll.set)
+
+# root.geometry("350x220")
+# root.title("PythonLobby.com")
+# root.mainloop()
